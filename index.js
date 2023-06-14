3bafb7e01311dd4e2991a4eabe5a9682b42bd02e
KenCorp <kennethwarui35@gmail.com>
2023-06-14 07:42:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+    function buttonClick(){
+        alert('Button clicked!');
+    }
+    button.addEventListener('click',buttonClick);
+
 }
+addingEventListener();
